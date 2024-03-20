d8664bf02a1db64c88c1ae2655de27206f3a3e82
mpirizdutra89 <163307010+mpirizdutra89@users.noreply.github.com>
2024-03-20 18:35:34 -0300

    rama martin

--- src/lab1/c2_tp2/pkg2024/LAB1C2_Tp22024.java
+++ src/lab1/c2_tp2/pkg2024/LAB1C2_Tp22024.java
@@ -1,12 +1,23 @@
 
 package lab1.c2_tp2.pkg2024;
 
-    //Agustin Jofre.
+//Agustin Jofre.
+
+/* 
+ * Clase Perro hereda de la clase Mascota
+
+ * @author: Martin Piriz, Agustin Jofre 
+
+ * @version: 1 20-03-24
+
+ * @see <a href = "https://github.com/aguz11/LAB1-C2_Tp2-2024-.git" /> Repositorio web </a>
+
+ */
 
 public class LAB1C2_Tp22024 {
 
     public static void main(String[] args) {
-    
+
     }
-    
+
 }
--- src/lab1/c2_tp2/pkg2024/Perro.java
+++ src/lab1/c2_tp2/pkg2024/Perro.java
@@ -1,6 +1,15 @@
 package lab1.c2_tp2.pkg2024;
 
-/* Autor Martin Nicolas Piriz Dutra */
+/* 
+ * Clase Perro hereda de la clase Mascota
+
+ * @author: Martin Nicolas Piriz Dutra
+
+ * @version: 1 20-03-24
+
+ * @see <a href = "https://github.com/aguz11/LAB1-C2_Tp2-2024-.git" /> Repositorio </a>
+
+ */
 public class Perro extends Mascotas {
     String comida;
 
